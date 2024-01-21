d801d6726aa7a55fc3d43130c0acd99bab692df6
Souravius1234 <sg.souravghosh2002@gmail.com>
2024-01-21 23:58:05 +0530

    Update Spacecraft_hello_world.py

--- example_usage/Spacecraft_hello_world.py
+++ example_usage/Spacecraft_hello_world.py
@@ -13,12 +13,20 @@ from Spacecraft import Spacecraft as SC
 from planetary_data import earth
 
 if __name__ == '__main__':
-	coes = [ earth[ 'radius' ] + 1000, 0.05, 30.0, 0.0, 0.0, 0.0 ]
-	sc   = SC(
+	coes1 = [ earth[ 'radius' ] + 10000, 0, 90.0, 0.0, 0.0, 0.0 ]
+	coes2 = [ earth[ 'radius' ] + 20000, 0, 60.0, 0.0, 0.0, 180.0 ]
+	sc1   = SC(
 			{
-			'coes'       : coes,
-			'tspan'      : '100', 
+			'coes'       : coes1,
+			'tspan'      : '1',
 			'dt'         : 100.0,
 			'orbit_perts': { 'J2': True }
 			} )
-	sc.plot_3d()
+	sc2   = SC(
+			{
+			'coes'       : coes2,
+			'tspan'      : '1',
+			'dt'         : 100.0,
+			'orbit_perts': { 'J2': True }
+			} )
+	sc1,sc2.plot_3d()
